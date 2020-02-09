6839ad5a01030dc18f4682c2a65d1d236e914714
i0nathan <i0nathan@outlook.com>
2020-02-09 17:24:28 -0600

    Modified PyGithub scripts

--- docs/python_docs/py-git-api/py-git-api_ex.py
+++ docs/python_docs/py-git-api/py-git-api_ex.py
@@ -12,6 +12,19 @@ def main():
 
     g = Github(username, password)
 
+    # Select a specific repo
+    # repo_name = input("Repo to download: ")
+
+    # or using an access token
+    # g = Github("access_token")
+
+    # Github Enterprise with custom hostname
+    # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
+    # Then play with your Github objects:
+    # for repo in g.get_user().get_repos():
+            # print(repo.name)
+
     # repo = g.get_user().get_repo(repo_name)
     repo = g.get_repo("i0nathan/code_snippets")
 
